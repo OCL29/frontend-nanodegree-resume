5166ea4572d71c89a15bf89109dd44cb7bb4e8de
Chun-Liang Ou <oujanliang29@gmail.com>
2015-12-02 10:30:15 +0800

    add  name and role to header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,11 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("OCL");
+var name = "OCL";
+var role = "programmer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
