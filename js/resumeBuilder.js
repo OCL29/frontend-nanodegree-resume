f871fe83a5352fd0d0c09ca8224488fa0b645d47
Chun-Liang Ou <oujanliang29@gmail.com>
2015-12-02 20:01:58 +0800

    add bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -10,3 +10,19 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+
+var bio = {
+	"name" : "OCL",
+	"role" : "programmer",
+	"contact_info" : {
+		"mobile" : "123-456-7890",
+		"email" : "123@gmail.com",
+		"github" : "OCL29",
+		"location" : "Tapei"
+	},
+	"bicPic" : "images/me.jpg",
+	"welcome_message" : "welcome to my website",
+	"skills" : [
+		"programming", "photograph", "sleep"
+	]
+};
